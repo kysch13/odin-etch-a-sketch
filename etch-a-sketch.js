c7111144876176f4322382d81e5a268f769c9d51
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-05-30 23:13:15 -0400

    Add polish to design, toggle switches

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,11 @@
 const container = document.getElementById('sketch-container');
-let tileCount = prompt("How big of a grid?");
+const controls = document.querySelector('.controls');
+let tileCount = 16;
+let rainbowMode = false;
+let darkenMode = false;
 
 drawGrid(tileCount);
+
 
 function createTile(i, j) {
     let tile = document.createElement('div');
@@ -28,11 +32,88 @@ function drawGrid(tiles) {
 }
 
 function colourTile(tile) {
-    tile.style.background = '#8a8b86';
+    if (rainbowMode) {
+        tile.style.background = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+
+        if (darkenMode) {
+            // Make tile progressivly darken with each pass
+            let tileOpacity = tile.style.opacity;
+            console.log(tileOpacity);
+            if (tileOpacity < 1) {
+                tileOpacity = (((tileOpacity * 10) + 1) / 10);
+                tile.style.opacity = `${tileOpacity}`;
+            }
+        } else {
+            tile.style.opacity = 1;
+        }
+        
+    } else {
+        tile.style.background = '#8a8b86';
+
+        if (darkenMode) {
+            tile.style.background = '#000';
+            // Make tile progressivly darken with each pass
+            let tileOpacity = tile.style.opacity;
+            console.log(tileOpacity);
+            if (tileOpacity < 1) {
+                tileOpacity = (((tileOpacity * 10) + 1) / 10);
+                tile.style.opacity = `${tileOpacity}`;
+            }
+        } else {
+            tile.style.opacity = 1;
+        }
+        
+
+    }
+}
+
+function rainbowToggle(rainbowSwitch) {
+    if (rainbowSwitch.checked) {
+        rainbowMode = true;
+    } else {
+        rainbowMode = false;
+    }
+}
+
+function darkenToggle(darkenSwitch) {
+    if (darkenSwitch.checked) {
+        darkenMode = true;
+    } else {
+        darkenMode = false;
+    }
+}
+
+function newGrid() {
+    container.innerHTML = '';
+    tileCount = prompt('How many rows and columns would you like in your grid? Please enter a number between 1 and 100.')
+    tileCount = Number(tileCount);
+    console.log(tileCount);
+    if (tileCount < 1 || tileCount > 100 || typeof tileCount != 'number' || isNaN(tileCount)) {
+        tileCount = 16
+        drawGrid(tileCount);
+        alert('Please enter a valid number bewtween 1 and 100.');
+    } else {
+        drawGrid(tileCount);
+    }
 }
 
 container.addEventListener('mouseover', function(e) {
-    console.log(e.target);
-    colourTile(e.target);
+    if (e.target.id != 'sketch-container') {
+        colourTile(e.target);
+    }
 });
+
+
+controls.addEventListener('click', function(e) {
+    let clickEvent = e.target;
+    if (clickEvent.id === 'rainbow-switch') {
+        rainbowToggle(clickEvent);
+    } else if (clickEvent.id === 'darken-switch') {
+        darkenToggle(clickEvent);
+    } else if (clickEvent.id === 'new-grid') {
+        newGrid();
+    }
+});
+
+
 
